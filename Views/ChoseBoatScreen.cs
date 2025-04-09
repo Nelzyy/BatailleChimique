290ceacb4032f355689ef3933975f6c88f5c6ca6
Chausette1 <3thanalleman@gmail.com>
2025-04-09 15:58:56 +0200

    ajout des bouttons

--- Views/ChoseBoatScreen.cs
+++ Views/ChoseBoatScreen.cs
@@ -6,25 +6,32 @@ namespace BatailleChimiqueWinform
         {
             InitializeComponent();
             Button[,] ChosenMatrix = new Button[8, 8];
+            InitializeBoard(ChosenMatrix);
+        }
 
+        private void InitializeBoard(Button[,] ChosenMatrix)
+        {
+            SuspendLayout();
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
                 {
+                    Color color = (i + j) % 2 == 0 ? Color.FromArgb(50, 50, 50) : Color.FromArgb(70, 70, 70);
                     Button button = new Button();
                     ChosenMatrix[i, j] = button;
-                    button.BackColor = Color.FromArgb(30, 30, 30);
-                    button.FlatAppearance.BorderColor = Color.FromArgb(30, 30, 30);
-                    button.FlatAppearance.BorderSize = 1;
-                    button.FlatStyle = FlatStyle.Popup;
+                    button.BackColor = color;
+                    button.FlatAppearance.BorderColor = Color.Black;
+                    button.FlatAppearance.BorderSize = 2;
+                    button.FlatStyle = FlatStyle.Flat;
                     button.ForeColor = Color.White;
-                    button.Location = new Point(, );
+                    button.Location = new Point(50 + (50 * j), 190 + (50 * i));
                     button.Size = new Size(50, 50);
                     button.Text = "";
                     button.UseVisualStyleBackColor = false;
-                    Controls.Add(ChosenMatrix[i, j]);
+                    Controls.Add(button);
                 }
             }
+            ResumeLayout();
         }
 
         private void Form1_Load(object sender, EventArgs e)
